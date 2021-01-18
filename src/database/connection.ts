3154cbe67284ccd781dc7bac27cb69425c11a1fa
Luis Carlos <luiscroxis@gmail.com>
2021-01-17 23:44:47 -0300

    fixx: Config Database Connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,7 +5,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host : process.env.PG_HOST,
-    port: process.env.PG_PORT || 5432,
+    port: 5432 || process.env.PG_PORT ,
     user : process.env.PG_USER,
     password : process.env.PG_PASSWORD,
     database : process.env.PG_DATABASE,
